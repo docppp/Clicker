81ab479d93f52bccb6379536b3ddb836703baf4d
docppp <29142757+docppp@users.noreply.github.com>
2021-03-28 14:54:30 +0200

    String based events creation.

--- scripts.py
+++ scripts.py
@@ -5,60 +5,67 @@ from events import Event, MouseEvent, TimeEvent
 
 class ScriptManager:
 
+    templates = {'mouse': {'x': int, 'y': int},
+                 'time': {'t': str, 'h': int, 'm': int, 's': int, 'u': int},
+                 'test': {'x': str, 'y': int, 'z': str, 'ff': float}}
+
     def __init__(self):
         self.name = "script"
         self.eventList = []
 
     def addEvent(self, event_string):
-        return validateEventString(event_string)
+        if self._validateEventString(event_string):
+            self.eventList.append(self._createEvent(event_string))
 
+    @staticmethod
+    def _validateEventString(event_string: str) -> bool:
+        event_type, *args = event_string.split(';')
+        args = ";".join(args)
+        if event_type not in ScriptManager.templates.keys():
+            return False
+        template = ScriptManager.templates.get(event_type)
+        return ScriptManager._validateArgs(args, template)
 
-def validateEventString(event_string: str) -> bool:
-    known_event_type = ["mouse", "time", "test"]
-
-    event_type, *rest = event_string.split(';')
-    rest = ";".join(rest)
-    if event_type not in known_event_type:
-        return False
-
-    template = {}
-    if event_type == "mouse":
-        template = {'x': int, 'y': int}
-    if event_type == "time":
-        template = {'t': str, 'h': int, 'm': int, 's': int, 'u': int}
-    if event_type == "test":
-        template = {'x': str, 'y': int, 'z': str, 'ff': float}
-
-    if not _validateArgs(rest, template):
-        return False
-
-    return True
-
-
-def _validateArgs(string: str, template: dict) -> bool:
-    try:
-        args = string.split(';')
-    except ValueError:
-        return False
-
-    keys = []
-    values = []
-
-    for arg in args:
-        try:
+    @staticmethod
+    def _createEvent(event_string: str) -> Event:
+        event_type, *args = event_string.split(';')
+        param_dict = {}
+        for arg in args:
             key, value = arg.split("=")
-        except ValueError:
-            return False
-        keys.append(key)
-        values.append(value)
+            param_dict[key] = value
 
-    if Counter(keys) != Counter(template.keys()):
-        return False
+        param_list = [ScriptManager.templates[event_type][p](param_dict[p])
+                      for p in ScriptManager.templates[event_type]]
 
-    for idx, value in enumerate(values):
+        e = Event.createEvent({'mouse': MouseEvent,
+                               'time': TimeEvent}[event_type], *param_list)
+        return e
+
+    @staticmethod
+    def _validateArgs(string: str, template: dict) -> bool:
         try:
-            template[keys[idx]](value)
+            args = string.split(';')
         except ValueError:
             return False
-    return True
+
+        keys = []
+        values = []
+
+        for arg in args:
+            try:
+                key, value = arg.split("=")
+            except ValueError:
+                return False
+            keys.append(key)
+            values.append(value)
+
+        if Counter(keys) != Counter(template.keys()):
+            return False
+
+        for idx, value in enumerate(values):
+            try:
+                template[keys[idx]](value)
+            except ValueError:
+                return False
+        return True
 
